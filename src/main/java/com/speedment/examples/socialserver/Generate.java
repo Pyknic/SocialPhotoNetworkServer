8cee2bd9069a1c7c8355b80d1a58a34a96e8148f
emilforslund <emil.forslund@pyknic.se>
2015-04-28 16:05:24 +0200

    Reset example

---
+++ src/main/java/com/speedment/examples/socialserver/Generate.java
@@ -0,0 +1,48 @@
+/**
+ *
+ * Copyright (c) 2006-2015, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.speedment.examples.socialserver;
+
+import com.speedment.orm.code.model.java.MainGenerator;
+import com.speedment.orm.config.model.Project;
+import com.speedment.orm.config.model.impl.utils.GroovyParser;
+import com.speedment.orm.gui.MainApp;
+import java.io.IOException;
+import java.nio.file.Paths;
+import static javafx.application.Application.launch;
+
+/**
+ *
+ * @author Emil Forslund
+ */
+public class Generate {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        boolean javaFx = true;
+
+        if (javaFx) {
+            launch(MainApp.class);
+        } else {
+            final Project p = GroovyParser.projectFromGroovy(Paths.get("src/main/groovy/", "speedment.groovy"));
+            new MainGenerator().accept(p);
+        }
+
+    }
+}
---
+++ src/main/java/com/speedment/examples/socialserver/Server.java
@@ -0,0 +1,76 @@
+package com.speedment.examples.socialserver;
+
+import fi.iki.elonen.ServerRunner;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.sql.Timestamp;
+import java.util.Optional;
+import java.util.Random;
+
+/**
+ *
+ * @author Emil Forslund
+ */
+public class Server extends ServerBase {
+
+    protected final Random random = new SecureRandom();
+
+    @Override
+    public String onRegister(String mail, String password) {
+        // TODO: Write register function.
+        return "false";
+    }
+
+    @Override
+    public String onLogin(String mail, String password) {
+        // TODO: Write login function.
+        return "false";
+    }
+
+    @Override
+    public String onSelf(String sessionKey) {
+        // TODO: Write self function.
+        return "false";
+    }
+
+    @Override
+    public String onUpload(String title, String description, String imgData, String sessionKey) {
+        // TODO: Write upload function.
+        return "false";
+    }
+
+    @Override
+    public String onFind(String freeText, String sessionKey) {
+        // TODO: Write find function.
+        return "false";
+    }
+
+    @Override
+    public String onFollow(long userId, String sessionKey) {
+        // TODO: Write follow function.
+        return "false";
+    }
+
+    @Override
+    public String onBrowse(String sessionKey, Optional<Timestamp> from, Optional<Timestamp> to) {
+        // TODO: Write browse function.
+        return "false";
+    }
+
+    @Override
+    public String onUpdate(String mail, String firstname, String lastName, Optional<String> avatar, String sessionKey) {
+        // TODO: Write update profile
+        return "false";
+    }
+
+    protected String nextSessionId() {
+        return new BigInteger(130, random).toString(32);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String... args) {
+        ServerRunner.run(Server.class);
+    }
+}
